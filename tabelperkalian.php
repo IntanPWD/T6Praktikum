cdebecc3342677786ea67d053b34c5d949c11db1
TugasPWD4 <putu92067@gmail.com>
2025-04-27 20:53:19 +0700

    Commit ketigaaa

--- tabelperkalian.php
+++ tabelperkalian.php
@@ -6,8 +6,33 @@ echo "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: 
 
 // Baris Judul Kolom
 echo "<tr>";
-echo "<td style='font-weight:bold;'>bilangan</td>"; // Kolom "bilangan" (judul) tanpa background (putih)
+echo "<td style='font-weight:bold;'>bilangan</td>"; // Kolom "bilangan" (judul)
 for ($i = 1; $i <= 10; $i++) {
-    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>"; // Header angka 1–10 warna hijau
+    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>"; // Header angka 1–10 
 }
 echo "</tr>";
+
+// Baris Isi Tabel
+for ($i = 1; $i <= 10; $i++) {
+    echo "<tr>";
+
+    // Kolom pertama tiap baris (angka bilangan) 
+    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>";
+
+    for ($j = 1; $j <= 10; $j++) {
+        $hasil = $i * $j;
+
+        if ($hasil % 2 != 0) {
+            $warna = "background-color:yellow;";
+        } else {
+            $warna = "background-color:cyan;";
+        }
+
+        echo "<td style='$warna'>$hasil</td>";
+    }
+
+    echo "</tr>";
+}
+
+echo"</table>";
+?>
