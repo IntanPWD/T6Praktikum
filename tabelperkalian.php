b7ec113be52bb94e739a73e4fdd2c07a90a5687c
TugasPWD4 <putu92067@gmail.com>
2025-04-27 20:37:32 +0700

    Commit 3

--- tabelperkalian.php
+++ tabelperkalian.php
@@ -9,3 +9,20 @@ for ($i = 1; $i <= 10; $i++) {
     echo "<td style='background-color:lime; font-weight:bold;'>$i</td>";
 }
 echo "</tr>";
+
+// Mengisi Tabel
+for ($i = 1; $i <= 10; $i++) {
+    echo "<tr>";
+    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>"; // Kolom bilangan samping
+    for ($j = 1; $j <= 10; $j++) {
+        $hasil = $i * $j;
+        if ($hasil % 3 == 0) {
+            $warna = "background-color:yellow;";
+        } else {
+            $warna = "background-color:cyan;";
+        }
+        
+        echo "<td style='$warna'>$hasil</td>";
+    }
+    echo "</tr>";
+}
