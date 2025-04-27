7d01654c697fe4bd8ea75462ac0e1615d7b6a34b
TugasPWD4 <putu92067@gmail.com>
2025-04-27 20:32:29 +0700

    Commit 1

--- tabelperkalian.php
+++ tabelperkalian.php
@@ -1,26 +1,3 @@
 <?php
-echo"<br><br>Tabel Perkalian:<br>";
-echo"<table border='1' cellpadding='5' cellspacing='0'
-style='border-collapse:collapse;'>";
-
-//Baris Untuk Judul Kolom
-echo"<tr>";
-echo"<td style='background-color:lime;font-weight:bold;'>Bilangan</td>";
-for($i=1; $i <= 10; $i++){
-    echo"<td style='background-color: lime; font-weight: bold;'>$i</td>";
-}
- echo"</tr>";
-
- //Mengisi Tabel
-for($i=1; $i <= 10; $i++){
-    echo"<tr>";
-    echo"<td style='background-color:lime;  font-weight: bold;'>$i</td>"; // Kolom bilangan yang ada di samping
-    for($j = 1; $j <= 10; $j++) {
-        echo "<td style='background-color: cyan;'>" . ($i * $j) . "</td>";
-    }
-    echo "</tr>";
-}
-
-echo "</table>";
-
-?>
+echo "<br><br>Tabel Perkalian:<br>";
+echo "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: collapse;'>";
