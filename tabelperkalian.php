26a4283c6219dbe76a0e125cf8b7c5bf2f54e53d
TugasPWD4 <putu92067@gmail.com>
2025-04-27 20:50:03 +0700

    Commit pertama

--- tabelperkalian.php
+++ tabelperkalian.php
@@ -1,28 +1,41 @@
 <?php
-echo "<br><br>Tabel Perkalian:<br>";
+echo "<br><br>";
+
+// Tabel Perkalian Matriks
 echo "<table border='1' cellpadding='5' cellspacing='0' style='border-collapse: collapse;'>";
 
-// Baris Untuk Judul Kolom
+// Baris Judul Kolom
 echo "<tr>";
-echo "<td style='background-color:lime; font-weight:bold;'>bilangan</td>";
+echo "<td style='font-weight:bold;'>bilangan</td>"; // Kolom "bilangan" (judul) tanpa background (putih)
 for ($i = 1; $i <= 10; $i++) {
-    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>";
+    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>"; // Header angka 1–10 warna hijau
 }
 echo "</tr>";
 
-// Mengisi Tabel
+// Baris Isi Tabel
 for ($i = 1; $i <= 10; $i++) {
     echo "<tr>";
-    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>"; // Kolom bilangan samping
+
+    // Kolom pertama tiap baris (angka bilangan) HIJAU
+    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>";
+
     for ($j = 1; $j <= 10; $j++) {
         $hasil = $i * $j;
-        if ($hasil % 3 == 0) {
+
+        // Warna cell:
+        // - Kuning untuk angka ganjil
+        // - Cyan untuk angka genap
+        if ($hasil % 2 != 0) {
             $warna = "background-color:yellow;";
         } else {
             $warna = "background-color:cyan;";
         }
-        
+
         echo "<td style='$warna'>$hasil</td>";
     }
+
     echo "</tr>";
 }
+
+echo "</table>";
+?>
