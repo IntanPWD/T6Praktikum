a7190c977bfb0fa39702eb210930cc8ac4797bd4
TugasPWD4 <putu92067@gmail.com>
2025-04-27 20:51:06 +0700

    Commit keduaaa

--- tabelperkalian.php
+++ tabelperkalian.php
@@ -11,31 +11,3 @@ for ($i = 1; $i <= 10; $i++) {
     echo "<td style='background-color:lime; font-weight:bold;'>$i</td>"; // Header angka 1–10 warna hijau
 }
 echo "</tr>";
-
-// Baris Isi Tabel
-for ($i = 1; $i <= 10; $i++) {
-    echo "<tr>";
-
-    // Kolom pertama tiap baris (angka bilangan) HIJAU
-    echo "<td style='background-color:lime; font-weight:bold;'>$i</td>";
-
-    for ($j = 1; $j <= 10; $j++) {
-        $hasil = $i * $j;
-
-        // Warna cell:
-        // - Kuning untuk angka ganjil
-        // - Cyan untuk angka genap
-        if ($hasil % 2 != 0) {
-            $warna = "background-color:yellow;";
-        } else {
-            $warna = "background-color:cyan;";
-        }
-
-        echo "<td style='$warna'>$hasil</td>";
-    }
-
-    echo "</tr>";
-}
-
-echo "</table>";
-?>
